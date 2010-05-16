8560890435c6bfa925498052c5acb593f4df81c6
Mirko Haaser <kontakt@mirko-haaser.de>
2010-05-16 13:56:30 +0200

    RaiseMan Projekt (Start in Kapitel 8)

---
+++ RaiseMan/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  RaiseMan
+//
+//  Created by Mirko Haaser on 16.05.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ RaiseMan/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RaiseMan
+//
+//  Created by Mirko Haaser on 16.05.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
